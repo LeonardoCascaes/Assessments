2d05ef6683b230fe5efe9ecbaa0da83a43922562
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2021-08-13 15:26:08 -0300

    SqlServer Integration Done Closed #7

---
+++ Assessments/Data/Context/BaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    class BaseContext : DbContext
+    {
+        public BaseContext() : base (@"Data Source=192.168.0.126;Initial Catalog=DbAssessments;Persist Security Info=True;User ID=Assessments;Password=assessments@123")
+        {
+
+        }
+    }
+}
