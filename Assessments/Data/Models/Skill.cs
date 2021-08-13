d6d38143644284f6ae88f6cfc574a247a077e3f2
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2021-08-13 13:46:46 -0300

    Create Skills Model Closed #5

---
+++ Assessments/Data/Models/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Skill : BaseModels
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public byte Grade { get; set; }
+    }
+}
