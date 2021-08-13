9fed5969440221a2875f0ee25ce85d017c479509
Eduardo Weber <edu.weber07@gmail.com>
2021-08-13 13:38:09 -0300

    ModelCriteria created Closed #8

---
+++ Assessments/Data/Models/Criteria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Criteria : BaseModels
+    {
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+}
