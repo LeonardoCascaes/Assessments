71ddb75d9341e891249dc1a5deecd3d51205b8b3
Eduardo Weber <edu.weber07@gmail.com>
2021-08-13 14:48:59 -0300

    Composition Phase and intermediate table created Closed #20

--- Assessments/Data/Models/Skill.cs
+++ Assessments/Data/Models/Skill.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Data.Models
 {
-    public class Skill : BaseModels
+    public class Skill : Base
     {
         public string Type { get; set; }
         public string Name { get; set; }
         public byte Grade { get; set; }
-        public List<Test> ListTest { get; set; }
         public Test Test { get; set; }
+        public List<Test> ListTest { get; set; }
     }
 }
